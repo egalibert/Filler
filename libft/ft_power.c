5f25076421aff16c9c6cd153fc6bbe88a5dfbd7a
Elliot Galibert <elliotgalibert@gmail.com>
2022-10-07 12:53:52 +0300

    libft

---
+++ libft/ft_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_power.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 14:31:25 by egaliber          #+#    #+#             */
+/*   Updated: 2022/10/06 20:06:36 by egaliber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long double	ft_power(int power, long double base)
+{
+	int	i;
+	int	number;
+
+	number = 1.0;
+	i = 0;
+	while (i < power)
+	{
+		number *= base;
+		i++;
+	}
+	return (number);
+}
---
+++ libft/ft_rounding.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rounding.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egaliber <egaliber@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 14:33:15 by egaliber          #+#    #+#             */
+/*   Updated: 2022/10/07 12:45:59 by egaliber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long double	ft_rounding(int i, long double num)
+{
+	long double	round;
+	long double	r;
+
+	r = 0.5;
+	while (i > 0)
+	{
+		r = r / 10.0;
+		i--;
+	}
+	round = num + r;
+	return (round);
+}
